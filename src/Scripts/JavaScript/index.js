1478d883063bec2b16fa925cd9f01f28b5a96a84
Mark <mcschaefer26@gmail.com>
2025-08-07 22:19:19 +0200

    Initial code testing dark mode toggle

--- src/Scripts/JavaScript/index.js
+++ src/Scripts/JavaScript/index.js
@@ -1,6 +1,18 @@
 // Generate the tasks cards and implement a edit function in which we update the object based on user input 
 import './tasks.js';
 import './add-task.js';
+import { toDoColumn } from './tasks.js';
+
+// Testing a dark mode toggle
+const lightDarkToggle = document.getElementById('light-dark-mode-toggle');
+lightDarkToggle.addEventListener('click', () => {
+    const body = document.getElementById('container-body');
+    body.classList.toggle('dark-mode'); // Can use a for each to target all elements with query selectorall
+    console.log(toDoColumn);
+    console.log('hello');
+    console.log(toDoColumn.querySelectorAll('.child'));
+    toDoColumn.querySelectorAll(':scope > *').forEach(el => el.classList.toggle('dark-mode-light')); // Might be easier to just give each created div a classname
+});
 
 
 
