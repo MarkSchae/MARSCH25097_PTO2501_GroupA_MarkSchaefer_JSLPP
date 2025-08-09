151b231427b48fa3dc5a939edebd0fde94f72185
Mark <mcschaefer26@gmail.com>
2025-08-09 13:51:28 +0200

    Initial code implementation and testing - side bar visibility toggle

--- src/Scripts/JavaScript/index.js
+++ src/Scripts/JavaScript/index.js
@@ -4,14 +4,31 @@ import './add-task.js';
 import { toDoColumn } from './tasks.js';
 
 // Testing a dark mode toggle
-const lightDarkToggle = document.getElementById('light-dark-mode-toggle');
-lightDarkToggle.addEventListener('click', () => {
+const lightDarkToggleBtn = document.getElementById('light-dark-mode-toggle');
+lightDarkToggleBtn.addEventListener('click', () => {
     const body = document.getElementById('container-body');
     body.classList.toggle('dark-mode'); // Can use a for each to target all elements with query selectorall
     console.log(toDoColumn);
     console.log('hello');
     console.log(toDoColumn.querySelectorAll('.child'));
     toDoColumn.querySelectorAll(':scope > *').forEach(el => el.classList.toggle('dark-mode-light')); // Might be easier to just give each created div a classname
+});
+
+// Testing hide the sidebar feature
+const sidebarToggleBtn = document.getElementById('side-bar-toggle');
+sidebarToggleBtn.addEventListener('click', () => {
+    const sideBar = document.getElementById('side-bar-div');
+    sideBar.classList.toggle('sm:hidden');
+    const mainGridTasks = document.getElementById('main-tasks-grid');
+    mainGridTasks.classList.toggle('sm:col-span-full');
+    const newSideBarToggleBtn = document.createElement('button');
+    newSideBarToggleBtn.innerHTML = 'SideBar';
+    newSideBarToggleBtn.addEventListener('click', () => {
+        sideBar.classList.toggle('sm:hidden');
+        mainGridTasks.classList.toggle('sm:col-span-full');
+        newSideBarToggleBtn.classList = 'hidden';
+    });
+    mainGridTasks.append(newSideBarToggleBtn);
 });
 
 
